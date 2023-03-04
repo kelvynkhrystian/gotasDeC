c21073f8324ea4012147c5897837824360078279
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-03-03 22:37:51 -0300

    organizando-jsx-inicial

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <main>
+        <article>
+            <img src="logo" alt="logomarca gotas de chocolate" />
+        </article>
+        <div>
+            <img src="carregando" alt="carregando" />
+            <button>Iniciar pedido</button>
+        </div>
+    </main>
+  )
+}
+
+export default Home
---
+++ src/pages/Order1.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Order1() {
+  return (
+    <main>
+        <h1>1. Qual o tamanho que vc gostaria?</h1>
+        <article>
+          <section>
+            <p>250g</p>
+          </section>
+          <section>
+            <p>250g</p>
+          </section>
+        </article>
+        <article>
+          <button>Anterior</button>
+          <button>Próximo</button>
+        </article>
+    </main>
+  )
+}
+
+export default Order1
