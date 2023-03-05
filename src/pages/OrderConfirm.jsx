7e5b22b169b6f3201fd586806c307bec9e26c8d2
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-03-05 19:37:54 -0300

    add-context-total

--- src/pages/OrderConfirm.jsx
+++ src/pages/OrderConfirm.jsx
@@ -6,25 +6,31 @@ import OrderContext from '../context/OrderContext';
 
 function OrderConfirm() {
 
-  const { tamanho, casca, recheio, adicional, nome, pagamento, valor } = useContext(OrderContext)
+  const order = useContext(OrderContext);
+
+  const handleOptionChange = (option) => {
+    // console.log(option.target.value);
+    const inputName = option.target.value
+    order.nome = inputName
+  };
 
   // const negrito = '*Kelvyn*';
   // const italico = '_Kelvyn_';
   // const taxado = '*~texto~';
   // const quebraDelinha = %0a
 
-  console.log(tamanho);
+  // console.log(order.tamanho);
 
   const api = 'https://api.whatsapp.com/send?';
   const fone = '5598991054292';
   const title = '*Gotas de Chocolate*%0a%0a';
-  const name = `*Nome*: _${nome}_%0a`;
-  const size = `*Tamanho*: _${tamanho}_%0a`;
-  const shell = `*Casca*: _${casca}_%0a`;
-  const filling = `*Recheio*: _${recheio}_%0a`;
-  const additional = `*Adicional*: _${adicional}_%0a`;
-  const payment = `*Forma de pagamento*: _${pagamento}_%0a%0a`;
-  const value = `*Total*: R$ _${valor}_,00`;
+  const name = `*Nome*: _${order.nome}_%0a`;
+  const size = `*Tamanho*: _${order.tamanho}_%0a`;
+  const shell = `*Casca*: _${order.casca}_%0a`;
+  const filling = `*Recheio*: _${order.recheio}_%0a`;
+  const additional = `*Adicional*: _${order.adicional}_%0a`;
+  const payment = `*Forma de pagamento*: _${order.pagamento}_%0a%0a`;
+  const value = `*Total*: R$ _${order.valor}_,00`;
 
   const message = `${title}${name}${size}${shell}${filling}${additional}${payment}${value}`
   const url = `${api}phone=${fone}&text=${message}`;
@@ -37,38 +43,38 @@ function OrderConfirm() {
           <div>
             <h2>Tamanho</h2>
             <div>
-              <p>{`${tamanho}`}</p>
-              <p>{`${tamanho=== '150g' ? 'R$ 25' : 'R$ 35'}`}</p>
+              <p>{`${order.tamanho}`}</p>
+              <p>{`${order.tamanho=== '150g' ? 'R$ 25' : 'R$ 35'}`}</p>
             </div>
           </div>
           <div>
             <h2>Casca</h2>
             <div>
-              <p>{`${casca}`}</p>
+              <p>{`${order.casca}`}</p>
             </div>
           </div>
           <div>
             <h2>Recheio</h2>
             <div>
-              <p>{`${recheio}`}</p>
+              <p>{`${order.recheio}`}</p>
             </div>
           </div>
           <div>
             <h2>Adicionais</h2>
             <div>
-              <p>{`${adicional}`}</p>
-              <p>{`${adicional=== 'Nenhum' ? 'R$ 0' : 'R$ 5'}`}</p>
+              <p>{`${order.adicional}`}</p>
+              <p>{`${order.adicional=== 'Nenhum' ? 'R$ 0' : 'R$ 5'}`}</p>
             </div>
           </div>
           <div>
-            <h1>{`Valor total:`} <span>{`R$ ${valor},00`}</span></h1>
+            <h1>{`Valor total:`} <span>{`R$ ${order.valor},00`}</span></h1>
           </div>
         </section>
 
         
         <h3>Antes de efetuar o pedido, diga seu nome para que possamos lhe atender melhor</h3>
 
-        <input type="text" placeholder='digite seu nome'/>
+        <input type="text" placeholder='digite seu nome' onChange={handleOptionChange}/>
       </article>
 
       <article>
