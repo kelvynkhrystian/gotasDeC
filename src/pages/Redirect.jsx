cf2a1e4a0abed1c9a6db3871275f160a899b8690
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-03-13 04:20:50 -0300

    test-italico-valor-total

--- src/pages/Redirect.jsx
+++ src/pages/Redirect.jsx
@@ -8,7 +8,7 @@ function Redirect() {
   const [seconds, setSeconds] = useState(10);
 
   const fone = '5598991054292';
-  const message = `*Gotas de Chocolate*%0a%0a*Nome*: _${order.nome}_%0a*Tamanho*: _${order.tamanho}_%0a*Casca*: _${order.casca}_%0a*Recheio*: _${order.recheio}_%0a*Adicional*: _${order.adicional}_%0a*Forma de pagamento*: _${order.pagamento}_%0a%0a*Total*: R$ _${order.valor}_,00`;
+  const message = `*Gotas de Chocolate*%0a%0a*Nome*: _${order.nome}_%0a*Tamanho*: _${order.tamanho}_%0a*Casca*: _${order.casca}_%0a*Recheio*: _${order.recheio}_%0a*Adicional*: _${order.adicional}_%0a*Forma de pagamento*: _${order.pagamento}_%0a%0a*Total*: _R$ ${order.valor},00_`;
 
   useEffect(() => {
     const interval = setInterval(() => {
